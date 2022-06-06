96f8f9b4bcfecf7339222bd9019842d07e23f6fa
Sharif Al Hosafy <sci.sharifismail2018@alexu.edu.eg>
2022-06-06 10:32:41 +0200

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,35 @@
-require('dotenv').config() // enviroment vars
-const express = require('express')
-const app = express()
+require('dotenv').config(); // enviroment vars
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
 
 // importing routes
-const usersRoutes = require('./src/entities/users/user.route')
+const usersRoutes = require('./src/entities/users/user.route');
 
 // app middlewares
-app.use(express.json())
+app.use(express.json());
 
 // database connection
-const db = require('./src/config/db')
+const db = require('./src/config/db');
 
 // app routes
-app.use('/api/v1/users', usersRoutes)
+app.use('/api/v1/users', usersRoutes);
+
+// error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.status || 500)
+    .send({ status: 'Error', message: error.message });
+});
 
 // starting server
 app.listen(process.env.port || 5000, (req, res) => {
-  console.log('Web Server is listening at port ' + (process.env.port || 5000))
-})
+  console.log('Web Server is listening at port ' + (process.env.port || 5000));
+});
