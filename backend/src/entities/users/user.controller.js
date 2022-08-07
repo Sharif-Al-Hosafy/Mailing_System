06183e384807adae374c9319ef631d26ad9d2a6b
OmarKnows <=>
2022-08-07 12:29:52 +0200

    user authentication routes

--- backend/src/entities/users/user.controller.js
+++ backend/src/entities/users/user.controller.js
@@ -1,38 +1,40 @@
-const jwt = require("jsonwebtoken");
-const err = require("../../utils/createError");
-const db = require("../../config/db"); // database connection
+const jwt = require('jsonwebtoken');
+const err = require('../../utils/createError');
+const db = require('../../config/db'); // database connection
+const bcrypt = require('bcrypt');
+
+const insertUser = async (req, res) => {
+  const { username, password, dep_id } = req.body;
+  const salt = await bcrypt.genSaltSync(10);
+  const hash = await bcrypt.hashSync(password, salt);
+  let queryString = `insert into mail_system.users (username,pass,dep_id) values ('${username}','${hash}',${dep_id})`;
+  const user = await db.query(queryString).catch((err) => {
+    throw err;
+  });
+  res.status(200).json({ message: 'success' });
+};
 
 const login = async (req, res, next) => {
   const { username, password } = req.body;
 
-  if (!username || !password) throw err(400, "Username or password not found");
-
-  const id = new Date().getDate(); // just for example
-
-  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-
-  res.status(200).json({ message: "success", token });
-};
-
-const getUser = async (req, res) => {
-  let queryString = `select * from users`;
+  if (!username || !password) throw err(400, 'Username or password not found');
+  let queryString = `select id,username,pass from mail_system.users where username='${username}'`;
   const user = await db.query(queryString).catch((err) => {
     throw err;
   });
-  res.json(user);
-};
 
-const getdata = async (req, res) => {
-  let queryString = `select orgname,importid,summary,importdate from importdata where importid = 1000 ORDER BY importdate DESC;`;
-  const user = await db.query(queryString).catch((err) => {
-    throw err;
+  if (user.length == 0) throw err(404, 'Username not Found');
+  const isMatch = await bcrypt.compare(password, user[0].pass);
+
+  if (!isMatch) throw err(400, 'da5al el pass 3edel ya fashel yabnel 3ars');
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
   });
-  res.json(user);
+
+  res.status(200).json({ token });
 };
 
 module.exports = {
   login,
-  getUser,
+  insertUser,
 };
--- backend/src/entities/users/user.route.js
+++ backend/src/entities/users/user.route.js
@@ -1,9 +1,8 @@
-const router = require("express").Router();
-const { login, getUser, getdata } = require("./user.controller");
-const authMiddleware = require("../../utils/authMiddleware");
+const router = require('express').Router();
+const { login, insertUser } = require('./user.controller');
+const authMiddleware = require('../../utils/authMiddleware');
 
-router.route("/login").post(login);
-
-router.route("/").get(getUser);
+router.route('/login').post(login);
+router.route('/add').post(insertUser);
 
 module.exports = router;
