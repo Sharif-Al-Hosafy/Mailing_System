cfdd04e0e514aa2ae5f8c5378dd6185b148ec838
Sharif Al Hosafy <sci.sharifismail2018@alexu.edu.eg>
2022-08-09 14:35:26 +0200

    file distribution

--- backend/src/entities/files/file.controller.js
+++ backend/src/entities/files/file.controller.js
@@ -1,41 +1,41 @@
-const jwt = require('jsonwebtoken')
-const err = require('../../utils/createError')
-const db = require('../../config/db.mail') // database archieve
-const { Buffer } = require('buffer')
-const fs = require('fs')
-let staticID
+const jwt = require('jsonwebtoken');
+const err = require('../../utils/createError');
+const db = require('../../config/db.mail'); // database archieve
+const { Buffer } = require('buffer');
+const fs = require('fs');
+let staticID;
 
 const fileList = async (req, res) => {
-  let queryString = `select id,orgname,importid,summary,importdate from archieve.importdata where importid = ${req.params.id} ORDER BY importdate DESC;`
+  let queryString = `select id,orgname,importid,summary,importdate from archieve.importdata where importid = ${req.params.id} ORDER BY importdate DESC;`;
   const file = await db.query(queryString).catch((err) => {
-    throw err
-  })
-  res.status(200).json(file)
-}
+    throw err;
+  });
+  res.status(200).json(file);
+};
 
 const fileToDailyScreen = async (req, res) => {
   let queryString = `insert into mail_system.file (file_no, file_data, orgname, summary) 
   select imd.id,imf.pdffile,imd.orgname,imd.summary from archieve.importdata imd
   join archieve.importfile imf 
   on imd.id = imf.id
-  where imd.id = '${req.params.fileId}';`
+  where imd.id = '${req.params.fileId}';`;
   const file = await db.query(queryString).catch((err) => {
-    throw err
-  })
-  res.status(200).json(file)
-}
+    throw err;
+  });
+  res.status(200).json(file);
+};
 
 const openFile = async (req, res) => {
-  let queryString = `select file_data from mail_system.file where file_no='${req.params.fileId}';`
+  let queryString = `select file_data from mail_system.file where file_no='${req.params.fileId}';`;
   const file = await db.query(queryString).catch((err) => {
-    throw err
-  })
+    throw err;
+  });
 
-  staticID = req.params.fileId
-  let buff = file[0].file_data
+  staticID = req.params.fileId;
+  let buff = file[0].file_data;
 
   //fs.writeFileSync('src/pages/hi.txt', blob, { encoding: 'ascii' })
-  fs.writeFileSync('src/pages/sample.pdf', buff, { encoding: 'ascii' })
+  fs.writeFileSync('src/pages/sample.pdf', buff, { encoding: 'ascii' });
   // fs.writeFile(
   //   "src/pages/sample.pdf",
   //   blob,
@@ -49,47 +49,65 @@ const openFile = async (req, res) => {
   //     }
   //   }
   // );
-  return res.status(200).json({ buff })
-}
+  return res.status(200).json({ buff });
+};
+
+const sendFiles = async (req, res) => {
+  let checkedDeps = req.body.checkedState;
+  let fileId = req.body.selectedFile;
+  let departments = [];
+
+  for (let i = 1; i < checkedDeps.length; i++)
+    if (checkedDeps[i] == true) departments.push(i);
+
+  departments.forEach(async (dep) => {
+    let queryString = `insert into mail_system.dep_file (file_id,dep_id) values ('${fileId}',${dep})`;
+    await db.query(queryString).catch((err) => {
+      throw err;
+    });
+  });
+
+  res.status(200).json({ message: 'success' });
+};
 
 const showDailyDocuments = async (req, res) => {
-  let queryString = `select * from mail_system.file`
+  let queryString = `select * from mail_system.file`;
   const files = await db.query(queryString).catch((err) => {
-    throw err
-  })
-  res.status(200).json(files)
-}
+    throw err;
+  });
+  res.status(200).json(files);
+};
 
 const getPdfEditor = (req, res) => {
   fs.readFile('src/pages/DocScreen.html', 'utf8', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    res.send(data)
-  })
-}
+    res.send(data);
+  });
+};
 
 const getPdf = (req, res) => {
   fs.readFile('src/pages/sample.pdf', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    res.send(data)
-  })
-}
+    res.send(data);
+  });
+};
 
 const savePdf = async (req, res) => {
-  let data = new Buffer.from(req.body.body, 'binary')
+  let data = new Buffer.from(req.body.body, 'binary');
   var query = ` update mail_system.file SET ? where file_no='${staticID}'`,
     values = {
       file_data: data,
-    }
+    };
   db.query(query, values, function (er, da) {
-    if (er) throw er
-  })
-}
+    if (er) throw er;
+  });
+};
 
 module.exports = {
   fileToDailyScreen,
@@ -99,4 +117,5 @@ module.exports = {
   getPdf,
   getPdfEditor,
   savePdf,
-}
+  sendFiles,
+};
--- backend/src/entities/files/file.route.js
+++ backend/src/entities/files/file.route.js
@@ -1,4 +1,4 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
   fileToDailyScreen,
   fileList,
@@ -7,16 +7,18 @@ const {
   getPdf,
   getPdfEditor,
   savePdf,
-} = require('./file.controller')
-const authMiddleware = require('../../utils/authMiddleware')
+  sendFiles,
+} = require('./file.controller');
+const authMiddleware = require('../../utils/authMiddleware');
 
-router.route('/savepdf').post(savePdf)
-router.route('/editor').get(getPdfEditor)
-router.route('/sample.pdf').get(getPdf)
-router.route('/:id').get(fileList) // list of document
-router.route('/daily/save/:fileId').post(fileToDailyScreen) // add document to daily screen
-router.route('/daily/show').get(showDailyDocuments) // show documents from daily screen
+router.route('/send').post(sendFiles);
+router.route('/savepdf').post(savePdf);
+router.route('/editor').get(getPdfEditor);
+router.route('/sample.pdf').get(getPdf);
+router.route('/:id').get(fileList); // list of document
+router.route('/daily/save/:fileId').post(fileToDailyScreen); // add document to daily screen
+router.route('/daily/show').get(showDailyDocuments); // show documents from daily screen
 
-router.route('/open/:fileId').get(openFile) // open a document
+router.route('/open/:fileId').get(openFile); // open a document
 
-module.exports = router
+module.exports = router;
