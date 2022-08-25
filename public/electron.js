181be102e63708ba32f027605f5fae2eb93dafdc
OmarKnows <omarknows98@gmail.com>
2022-08-25 10:50:02 +0200

    Mailing System v1.1 alpha

--- public/electron.js
+++ public/electron.js
@@ -1,47 +1,54 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const isDev = require("electron-is-dev");
-const server = require("../backend/server");
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+const isDev = require('electron-is-dev')
+// const server = require("../backend/server");
+// const { fork } = require('child_process')
+// const serverProcess = fork(path.resolve(__dirname, './backend/server.js'))
 
-let mainWindow;
+// try {
+//   serverProcess.stdout.on('data', console.log)
+//   serverProcess.stderr.on('data', console.error)
+// } catch (e) {}
+
+let mainWindow
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: "",
+    icon: './icon.png',
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
       contextIsolation: true,
     },
-  });
-  mainWindow.setMenu(null);
+  })
+  //mainWindow.setMenu(null)
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  mainWindow.on("closed", () => (mainWindow = null));
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  mainWindow.on('closed', () => (mainWindow = null))
 }
 
 electron.protocol.registerSchemesAsPrivileged([
   {
-    scheme: "file",
+    scheme: 'file',
     privileges: { secure: true, standard: true },
   },
-]);
+])
 
-app.on("ready", createWindow);
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
